d585dac4de6817511dd384434afe494c8f00a5d6
Spencer Dent <sd00070@my.westga.edu>
2021-08-25 18:41:46 -0400

    fill TileRack at game start & display Tile characters using CellFactory

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -3,18 +3,22 @@ package edu.westga.cs.babble.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import edu.westga.cs.babble.model.EmptyTileBagException;
 import edu.westga.cs.babble.model.PlayedWord;
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
 import edu.westga.cs.babble.model.TileRack;
+import edu.westga.cs.babble.model.TileRackFullException;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
 /**
  * Serves as the controller for the Babble Game Application.
@@ -36,13 +40,18 @@ public class BabbleController implements Initializable {
 	private final IntegerProperty scoreValue;
 
 	/* communicating with the view */
-	@FXML private ListView<Tile> tileListView;
-	@FXML private ListView<Tile> selectedLetterListView;
+	@FXML
+	private ListView<Tile> tileListView;
+	@FXML
+	private ListView<Tile> selectedLetterListView;
 
-	@FXML private Button resetButton;
-	@FXML private Button playWordButton;
+	@FXML
+	private Button resetButton;
+	@FXML
+	private Button playWordButton;
 
-	@FXML private TextField scoreTextField;
+	@FXML
+	private TextField scoreTextField;
 
 	/**
 	 * Constructor: initializes all of the instance variables, and prepares the
@@ -56,8 +65,9 @@ public class BabbleController implements Initializable {
 		this.wordDictionary = new WordDictionary();
 
 		this.tileBag = new TileBag();
-
+		
 		this.tileRack = new TileRack();
+		this.fillTileRackWithTilesFromTileBag();
 
 		this.playedWord = new PlayedWord();
 
@@ -72,6 +82,28 @@ public class BabbleController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 
 		this.scoreTextField.textProperty().bind(this.scoreValue.asString());
+		
+		this.tileListView.setItems(this.tileRack.tiles());
+		this.tileListView.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+			@Override
+			public ListCell<Tile> call(ListView<Tile> list) {
+				return new TileCell();
+			}
+		});
+	}
+	
+	static class TileCell extends ListCell<Tile> {
+		@Override
+		public void updateItem(Tile tile, boolean empty) {
+			super.updateItem(tile, empty);
+			
+			if (empty || tile == null) {
+				this.setText(null);
+				this.setGraphic(null);
+			} else {				
+				this.setText(tile.getLetter() + "");
+			}
+		}
 	}
 
 	/**
@@ -104,5 +136,16 @@ public class BabbleController implements Initializable {
 	public void handlePlayWordButtonClicked(MouseEvent clickEvent) {
 		/* TODO: write code to handle click event */
 		this.scoreValue.set(this.scoreValue.get() + 1);
+	}
+
+	private void fillTileRackWithTilesFromTileBag() {
+		int numberOfTilesNeeded = this.tileRack.getNumberOfTilesNeeded();
+
+		for (int index = 0; index < numberOfTilesNeeded; index++) {
+			try {
+				this.tileRack.append(this.tileBag.drawTile());
+			} catch (TileRackFullException | EmptyTileBagException exception) {
+			}
+		}
 	}
 }
