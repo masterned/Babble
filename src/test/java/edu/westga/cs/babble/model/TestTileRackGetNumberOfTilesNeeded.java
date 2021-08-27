160f7a214fc1c34440339be1ebeb939b08bb06fc
Spencer Dent <sd00070@my.westga.edu>
2021-08-27 00:25:46 -0400

    test TileRack's getNumberOfTilesNeeded method

--- src/test/java/edu/westga/cs/babble/model/TestTileRackGetNumberOfTilesNeeded.java
+++ src/test/java/edu/westga/cs/babble/model/TestTileRackGetNumberOfTilesNeeded.java
@@ -2,32 +2,68 @@ package edu.westga.cs.babble.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Test TileRack's getNumberOfTilesNeeded method.
  * 
  * @author Spencer Dent
  * @version 2021-08-26
  */
 public class TestTileRackGetNumberOfTilesNeeded {
 
+	private TileRack testTileRack;
+
+	/**
+	 * Before each test, create a new TileRack to use.
+	 */
+	@BeforeEach
+	public void initEach() {
+		this.testTileRack = new TileRack();
+	}
+
+	/**
+	 * An empty TileRack has all of its spaces available.
+	 */
 	@Test
 	public void emptyTileRackShouldNeedMaxSizeNumberOfTiles() {
-		fail("Not yet implemented");
+		assertEquals(TileRack.MAX_SIZE, this.testTileRack.getNumberOfTilesNeeded());
 	}
-	
+
+	/**
+	 * A TileRack with only one Tile has all but one of its spaces available.
+	 */
 	@Test
 	public void tileRackWithOneTileShoudNeedMaxSizeMinusOneTiles() {
-		fail("Not yet implemented");
+		this.testTileRack.append(new Tile('A'));
+
+		assertEquals(TileRack.MAX_SIZE - 1, this.testTileRack.getNumberOfTilesNeeded());
 	}
-	
+
+	/**
+	 * A TileRack with n Tiles in it has its total number of spaces minus n
+	 * available.
+	 */
 	@Test
 	public void tileRackWithSeveralTilesShouldNeedSomeTiles() {
-		fail("Not yet implemented");
+		this.testTileRack.append(new Tile('O'));
+		this.testTileRack.append(new Tile('T'));
+		this.testTileRack.append(new Tile('T'));
+		this.testTileRack.append(new Tile('F'));
+
+		assertEquals(TileRack.MAX_SIZE - 4, this.testTileRack.getNumberOfTilesNeeded());
 	}
-	
+
+	/**
+	 * A full TileRack has no spaces available.
+	 */
 	@Test
 	public void fullTileRackNeedsZeroTiles() {
-		fail("Not yet implemented");
+		for (int counter = 0; counter < TileRack.MAX_SIZE; counter++) {
+			this.testTileRack.append(new Tile('A'));
+		}
+
+		assertEquals(0, this.testTileRack.getNumberOfTilesNeeded());
 	}
 }
