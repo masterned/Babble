c14409ffb1640516ad2d001a5d498864b8785ccf
Spencer Dent <sd00070@my.westga.edu>
2021-08-27 00:46:06 -0400

    test PlayedWord's clear method

--- src/test/java/edu/westga/cs/babble/model/TestPlayedWordClear.java
+++ src/test/java/edu/westga/cs/babble/model/TestPlayedWordClear.java
@@ -1,28 +1,61 @@
 package edu.westga.cs.babble.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Tests PlayedWord's clear method.
  * 
  * @author Spencer Dent
  * @version 2021-08-26
  */
 public class TestPlayedWordClear {
 
-	@Test
-	public void shouldClearEmptyWord(){
-		fail("Not yet implemented");
+	private PlayedWord testPlayedWord;
+
+	/**
+	 * Before each test, create a new PlayedWord object.
+	 */
+	@BeforeEach
+	public void initEach() {
+		this.testPlayedWord = new PlayedWord();
 	}
-	
+
+	/**
+	 * Clearing an empty word should result in an empty word (no change).
+	 */
 	@Test
-	public void shouldClearWordWithOneTile(){
-		fail("Not yet implemented");
+	public void shouldClearEmptyWord() {
+		this.testPlayedWord.clear();
+		assertEquals(0, this.testPlayedWord.tiles().size());
 	}
-	
+
+	/**
+	 * Should be able to clear a PlayedWord consisting of a single Tile.
+	 */
 	@Test
-	public void shouldClearWordWithManyTiles(){
-		fail("Not yet implemented");
+	public void shouldClearWordWithOneTile() {
+		this.testPlayedWord.append(new Tile('A'));
+
+		this.testPlayedWord.clear();
+
+		assertEquals(0, this.testPlayedWord.tiles().size());
+	}
+
+	/**
+	 * Should be able to clear a PlayedWord consisting of multiple Tiles.
+	 */
+	@Test
+	public void shouldClearWordWithManyTiles() {
+		this.testPlayedWord.append(new Tile('M'));
+		this.testPlayedWord.append(new Tile('A'));
+		this.testPlayedWord.append(new Tile('N'));
+		this.testPlayedWord.append(new Tile('Y'));
+
+		this.testPlayedWord.clear();
+
+		assertEquals(0, this.testPlayedWord.tiles().size());
 	}
 }
